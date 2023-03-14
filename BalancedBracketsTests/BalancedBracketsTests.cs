cb37c342237277f6df2e2313b5a97f09cc2fa81d
ngohile <ngohile.yakubu@gmail.com>
2023-03-14 16:46:05 -0500

    test brackets

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -1,13 +1,61 @@
-﻿namespace BalancedBracketsTests;
+﻿using BalancedBrackets;
 
-[TestClass]
-public class BalancedBracketsTests
+namespace BalancedBracketsTests
 {
-    // TODO: Add tests to this file.
+    using BalancedBrackets;
 
-    [TestMethod]
-    public void EmptyTest()
+    [TestClass]
+    public class BalancedBracketsTests
     {
-        Assert.AreEqual(true, true);
+        // TODO: Add tests to this file.
+
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod]
+        public void OnlyBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
+
+        [TestMethod]
+        public void StringAndBracketsReturnsTrue() 
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("launch[Code]"));
+        }
+        [TestMethod]
+        public void BracketsBeforeStringReturnTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]launchCode"));
+        }
+        [TestMethod]
+        public void EmptyStringReturnTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(""));
+        }
+        [TestMethod]
+        public void SingleBracketWithStringReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[launchCode"));
+        }
+        [TestMethod]
+        public void ReversedBracketsWithStringReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("launch]Code["));
+        }
+        [TestMethod]
+        public void SingleBracketsWithStringReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("launchCode["));
+        }
+        [TestMethod]
+        public void OpeningBracketsReturnTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]launchCode"));
+        }
     }
+
 }
